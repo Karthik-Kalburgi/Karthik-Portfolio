06d4136516e741f62e84e11fbca6ba49cb638957
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2024-11-19 22:44:15 +0530

    Added Framer Motion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
 import Techonologies from "./Components/Techonologies";
+import Intern from "./Components/Intern";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
 
 const App = () => {
   return (
@@ -16,6 +19,9 @@ const App = () => {
         <Hero />
         <About />
         <Techonologies />
+        <Intern />
+        <Projects />
+        <Contact />
       </div>
     </div>
   );
--- src/Components/Techonologies.jsx
+++ src/Components/Techonologies.jsx
@@ -7,37 +7,82 @@ import { FaHtml5 } from "react-icons/fa";
 import { SiCsswizardry } from "react-icons/si";
 import { FaBootstrap } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
+import {motion} from 'framer-motion'
 
-
+const iconVariants = (duration) =>({
+    initial:{y:-10},
+    animate:{
+        y:[10,-10],
+        transition:{
+            duration:duration,
+            ease:"linear",
+            repeat:Infinity,
+            repeatType:"reverse"
+        }
+    }
+})
 const Techonologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24 ">
       <h1 className="my-20 text-center text-4xl ">Techonologies</h1>
       <div className="flex flex-wrap items-center justify-center gap-4 ">
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        <motion.div 
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate"
+
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
         <   FaReact className="text-7xl text-cyan-400" /> 
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(3.5)}
+         initial="initial"
+         animate="animate"
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
           <RiNextjsFill  className="text-7xl " />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(1.5)}
+         initial="initial"
+         animate="animate"
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
           <SiJavascript  className="text-7xl text-yellow-300" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(4)}
+         initial="initial"
+         animate="animate"
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
           <TbBrandMongodb  className="text-7xl text-green-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2)}
+         initial="initial"
+         animate="animate"
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
           <FaHtml5  className="text-7xl text-red-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(1.5)}
+         initial="initial"
+         animate="animate"
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
           <SiCsswizardry  className="text-7xl text-yellow-300" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        </motion.div>
+        <motion.div
+         variants={iconVariants(4)}
+         initial="initial"
+         animate="animate"
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
           <FaBootstrap  className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-900 p-4 ">
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial="initial"
+         animate="animate"
+        className="rounded-2xl border-4 border-neutral-900 p-4 ">
           <FaPython  className="text-7xl text-yellow-500" />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
